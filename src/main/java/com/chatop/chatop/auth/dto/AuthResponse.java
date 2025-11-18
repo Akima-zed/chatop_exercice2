af7d6170ee63cf0a8c4824758e5f13ddb0fff774
julie <julie.regereau@gmail.com>
2025-11-18 17:08:04 +0100

    création des dto

--- src/main/java/com/chatop/chatop/auth/dto/AuthResponse.java
+++ src/main/java/com/chatop/chatop/auth/dto/AuthResponse.java
@@ -1,4 +1,10 @@
 package com.chatop.chatop.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
+    private String token;
 }
--- src/main/java/com/chatop/chatop/auth/dto/RegisterRequest.java
+++ src/main/java/com/chatop/chatop/auth/dto/RegisterRequest.java
@@ -1,4 +1,13 @@
 package com.chatop.chatop.auth.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
 }
