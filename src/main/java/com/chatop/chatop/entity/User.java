2b1c8aff166c3e9595dc6e341756bf842d5df1cd
julie <julie.regereau@gmail.com>
2025-11-18 17:05:41 +0100

    création table USER

--- src/main/java/com/chatop/chatop/entity/User.java
+++ src/main/java/com/chatop/chatop/entity/User.java
@@ -1,4 +1,27 @@
 package com.chatop.chatop.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
