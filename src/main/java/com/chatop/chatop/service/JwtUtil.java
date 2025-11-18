079e05efed00cac07b6ae6d8720f623ebf310c7e
julie <julie.regereau@gmail.com>
2025-11-18 17:17:16 +0100

    modif du pom ,manque dependence jjwt

--- src/main/java/com/chatop/chatop/service/JwtUtil.java
+++ src/main/java/com/chatop/chatop/service/JwtUtil.java
@@ -1,7 +1,8 @@
 package com.chatop.chatop.service;
 
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
