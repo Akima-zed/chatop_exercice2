a111dfe83eaa0c30a762bedff7fa6b9425436861
julie <julie.regereau@gmail.com>
2025-11-18 16:53:40 +0100

    création des different dossier

--- src/main/java/com/chatop/ChatopApplication.java
+++ src/main/java/com/chatop/ChatopApplication.java
@@ -1,4 +1,4 @@
-package com.chatop.demo;
+package com.chatop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/chatop/ChatopApplicationTests.java
+++ src/test/java/com/chatop/ChatopApplicationTests.java
@@ -1,4 +1,4 @@
-package com.chatop.demo;
+package com.chatop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
